6d2251c20ec532a0739da4bbe30f1f13ab26047d
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-05-05 12:52:43 +0200

    Started to work on navbar, header and logo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -9,7 +9,27 @@ import menuItems from './header.data';
 
 export default function Header({ className }) {
   return (
-      <h1>Header</h1>
+      <header sx={styles.header} className={className} id="header">
+        <Container sx={styles.container}>
+          <Logo src={LogoDark} />
+          <Flex as="nav" sx={styles.nav}>
+            {menuItems.map((item, index) => (
+              <Link
+                activeClass="active"
+                to={item.path}
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                key={index}
+              >
+                {item.label}
+              </Link>
+            ))}
+          </Flex>
+          <Button className="donate__btn" variant="secondary" aria-label="Get Started">Get Started</Button>
+        </Container>
+      </header>
   );
 }
 
