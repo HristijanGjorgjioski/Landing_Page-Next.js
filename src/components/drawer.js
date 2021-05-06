014d2a2f45434c5791645e690f97ff6040115cb4
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-05-06 13:24:14 +0200

    Header is done

--- src/components/drawer.js
+++ src/components/drawer.js
@@ -1,6 +1,6 @@
-import React, { Fragment } from 'react';
-import { Box } from 'theme-ui';
-import RcDrawer from 'rc-drawer';
+import React, { Fragment } from 'react'
+import { Box } from 'theme-ui'
+import RcDrawer from 'rc-drawer'
 
 export default function Drawer ({ 
   className,
@@ -29,13 +29,19 @@ export default function Drawer ({
         duration={'0.4s'}
         { ...props }
       >
-
+        {closeButton && (
+          <Box as="div" onClick={toggleHandler} sx={closeBtnStyle}>{closeButton}</Box>
+        )}
+        <Box sx={drawerStyle}>{children}</Box>
       </RcDrawer>
+      <Box className="drawer__handler" style={{ display: 'inline-block' }} onClick={toggleHandler}>
+        {drawerHandler}
+      </Box>
     </Fragment>
-  );
-};
+  )
+}
 
 Drawer.defaultProps = {
   width: '320px',
   placement: 'left',
-};
+}
