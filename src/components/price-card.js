0f995ef1f2d3580e3ba2361e95c9ced495308b93
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-05-08 11:06:53 +0200

    Pricing section is done, next is Team section

--- src/components/price-card.js
+++ src/components/price-card.js
@@ -21,7 +21,26 @@ export default function PriceCard({
           <Heading className="package__name" variant="title">{name}</Heading>
           <Text as="p">{description}</Text>
         </Box>
-        <List />
+        <List items={points} childStyle={styles.listItem} />
+        <Text className="package__price" sx={styles.price}>
+          {priceWithUnit}
+          <span>/Monthly</span>
+        </Text>
+        <Box sx={styles.buttonGroup}>
+          <Button variant="primary" aria-label={buttonText}>
+            {buttonText}
+          </Button>
+          {anotherOption && (
+            <Button
+              variant="textButton"
+              className="free__trial"
+              aria-label={anotherOption}
+              sx={{ color: 'black' }}
+            >
+              {anotherOption}
+            </Button>
+          )}
+        </Box>
       </Box>
     </Card>
   );
